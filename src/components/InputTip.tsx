30e9d09d8db7c3eea8c8cddd6bcab88f8646a192
Andriw Tapanes <andriwtapanes@gmail.com>
2023-06-13 23:45:14 -0400

    added reset button and more styling

--- src/components/InputTip.tsx
+++ src/components/InputTip.tsx
@@ -99,115 +99,131 @@ const InputTip = () => {
     }
   };
 
-  // const share = () => {
-  //   alert("The URL of this page is: " + window.location.href);
-  // };
+  const reset = () => {
+    setTotal("");
+    setRating(0);
+    setPercent("0.20");
+    setPeople("");
+    setTip("");
+    setNetTotal("");
+    setNetTotalPerPerson("");
+    setUrl("https://tiptapcalculator.netlify.app/");
+  };
 
   return (
-    <div className="flex flex-row items-end ml-12">
-      <div className="flex flex-col justify-center items-center mt-4 mb-6 bg-gray-400 rounded-3xl border-4 border-white shadow-inner">
-        <div className="flex flex-col justify-center items-center rounded-t-3xl bg-gradient-to-t from-purple-700 to-cyan-500 p-6 pb-4 shadow-lg">
-          <h2 className="text-xl">Welcome to</h2>
-          <h1 className="text-6xl">Tip-Tap!</h1>
-          <div className="flex flex-col flex-center items-center main-h-screen pt-10">
-            <div className="flex items-center">
-              <div>
-                <h2>Bill total</h2>
-                <div className="flex">
-                  <p className="pr-2 text-3xl">$</p>
+    <div className="flex items-end ml-12">
+      <div className="flex flex-col">
+        <div className="flex flex-col justify-center items-center mt-4 mb-4 bg-slate-500 rounded-3xl border-b-2 border-r-2 border-white shadow-inner">
+          <div className="flex flex-col justify-center items-center rounded-t-3xl bg-gradient-to-t from-purple-700 to-cyan-500 p-6 pt-2 pb-4 shadow-xl">
+            <h2 className="text-xl">Welcome to</h2>
+            <h1 className="text-6xl">Tip-Tap!</h1>
+            <div className="flex flex-col flex-center items-center main-h-screen pt-10">
+              <div className="flex items-center">
+                <div>
+                  <h2 className="mb-2">Bill total</h2>
+                  <div className="flex">
+                    <p className="pr-2 text-3xl">$</p>
+                    <input
+                      className="text-white text-2xl bg-transparent border-b-2 mb-8 mr-4 h-10 w-52 outline-0"
+                      onKeyDown={handleKeyDown}
+                      onChange={handleTotalChange}
+                      value={total}
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col items-center">
+                  <h2 className="mb-2">Split</h2>
                   <input
-                    className="text-white text-2xl bg-transparent border-b-2 mb-8 mr-4 h-10 w-52 outline-0"
+                    className="w-10 text-white text-2xl bg-transparent border-b-2 mb-6 h-8 outline-0"
                     onKeyDown={handleKeyDown}
-                    onChange={handleTotalChange}
-                    value={total}
+                    onChange={handlePeopleChange}
+                    value={people}
                   />
                 </div>
               </div>
-              <div className="flex flex-col items-center">
-                <h2>Split</h2>
-                <input
-                  className="w-10 text-white text-2xl bg-transparent border-b-2 mb-6 h-8 outline-0"
-                  onKeyDown={handleKeyDown}
-                  onChange={handlePeopleChange}
-                  value={people}
+              <div className="flex flex-col items-center star-rating mb-4">
+                <h3 className="text-xs">Rate your experience</h3>
+                <StarRatingComponent
+                  name="rating"
+                  value={rating}
+                  onStarClick={(nextValue, prevValue) =>
+                    handleStarClick(nextValue, prevValue)
+                  }
+                  starCount={5}
+                  starColor={"#ffb400"}
+                  emptyStarColor={"#ccc"}
                 />
               </div>
             </div>
-            <div className="flex flex-col items-center star-rating mb-4">
-              <h3 className="text-xs">Rate your experience</h3>
-              <StarRatingComponent
-                name="rating"
-                value={rating}
-                onStarClick={(nextValue, prevValue) =>
-                  handleStarClick(nextValue, prevValue)
-                }
-                starCount={5}
-                starColor={"#ffb400"}
-                emptyStarColor={"#ccc"}
-              />
-            </div>
-          </div>
-          <button
-            className="bg-cyan-500 px-16 py-2 rounded-full shadow-lg border-b-2 transition delay-50 duration-200 ease-in-out hover:bg-teal-400 hover:border-t-2 hover:text-black hover:border-b-0 hover:border-black click:bg-teal-400"
-            onClick={calculate}
-          >
-            Calculate!
-          </button>
-        </div>
-        <div className="flex flex-col items-center">
-          <div
-            data-tooltip-target="tooltip-total"
-            className="flex flex-col items-center mt-4"
-          >
-            <h2 className="text-xs mr-4">Net Total</h2>
-            <p className="text-4xl">{netTotal}</p>
-            {/*TOOLTIP*/}
-            <div
-              id="tooltip-total"
-              role="tooltip"
-              className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+            <button
+              className="bg-cyan-500 px-16 py-2 rounded-full shadow-lg border-b-2 border-r-2 transition delay-50 duration-200 ease-in-out hover:bg-teal-300 hover:border-t-2 hover:text-black hover:border-b-0 hover:border-black click:bg-teal-400"
+              onClick={calculate}
             >
-              Bill total plus Tip total
-              <div className="tooltip-arrow" data-popper-arrow></div>
-            </div>
-            {/*TOOLTIP*/}
+              Calculate!
+            </button>
           </div>
-          <div
-            data-tooltip-target="tooltip-split-tip-total"
-            className="flex flex-col items-center mt-4"
-          >
-            <h2 className="text-xs mr-4">Split Tip</h2>
-            <p className="text-4xl">{tip}</p>
-            {/*TOOLTIP*/}
+          <div className="flex flex-col items-center">
             <div
-              id="tooltip-split-tip-total"
-              role="tooltip"
-              className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              data-tooltip-target="tooltip-total"
+              className="flex flex-col items-center mt-4"
             >
-              Tip total split between people
-              <div className="tooltip-arrow" data-popper-arrow></div>
+              <h2 className="text-xs mr-4">Net Total</h2>
+              <p className="text-4xl">{netTotal}</p>
+              {/*TOOLTIP*/}
+              <div
+                id="tooltip-total"
+                role="tooltip"
+                className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              >
+                Bill total plus Tip total
+                <div className="tooltip-arrow" data-popper-arrow></div>
+              </div>
+              {/*TOOLTIP*/}
             </div>
-            {/*TOOLTIP*/}
-          </div>
-          <div
-            data-tooltip-target="tooltip-split-net-total"
-            className="flex flex-col items-center mt-4 mb-4"
-          >
-            <h2 className="text-xs mr-4">Split Net Total</h2>
-            <p className="text-4xl">{netTotalPerPerson}</p>
-            {/*TOOLTIP*/}
             <div
-              id="tooltip-split-net-total"
-              role="tooltip"
-              className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              data-tooltip-target="tooltip-split-tip-total"
+              className="flex flex-col items-center mt-4"
             >
-              Bill total plus Tip total split between people
-              <div className="tooltip-arrow" data-popper-arrow></div>
+              <h2 className="text-xs mr-4">Split Tip</h2>
+              <p className="text-4xl">{tip}</p>
+              {/*TOOLTIP*/}
+              <div
+                id="tooltip-split-tip-total"
+                role="tooltip"
+                className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              >
+                Tip total split between people
+                <div className="tooltip-arrow" data-popper-arrow></div>
+              </div>
+              {/*TOOLTIP*/}
             </div>
-            {/*TOOLTIP*/}
+            <div
+              data-tooltip-target="tooltip-split-net-total"
+              className="flex flex-col items-center mt-4 mb-4"
+            >
+              <h2 className="text-xs mr-4">Split Net Total</h2>
+              <p className="text-4xl">{netTotalPerPerson}</p>
+              {/*TOOLTIP*/}
+              <div
+                id="tooltip-split-net-total"
+                role="tooltip"
+                className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+              >
+                Bill total plus Tip total split between people
+                <div className="tooltip-arrow" data-popper-arrow></div>
+              </div>
+              {/*TOOLTIP*/}
+            </div>
           </div>
         </div>
+        <button
+          className="bg-purple-700 px-16 py-2 rounded-full shadow-lg border-b-2 border-r-2 transition delay-50 duration-200 ease-in-out hover:bg-purple-400 hover:border-t-2 hover:text-black hover:border-b-0 hover:border-black click:bg-teal-400"
+          onClick={reset}
+        >
+          RESET
+        </button>
       </div>
+
       <div className="ml-4 mb-6 hover:cursor-pointer">
         <RWebShare
           data={{
